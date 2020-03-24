94fff9dec862df50ffe7a9eca402acc687867d9d
nya <home@cathand.org>
2020-03-24 16:42:29 +0900

    fixed: Auth header

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 		log.Errorf("Error: %s\n", err)
 		os.Exit(1)
 	}
-	req.Header.Set("token " + conf.AuthToken, "Authorization")
+	req.Header.Set("Authorization", "token " + conf.AuthToken)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
 
